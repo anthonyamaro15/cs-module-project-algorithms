0687a526187cc5ddacc1ead7a7568a2c9367573b
anthony amaro <anthonyamaro@anthonys-MacBook-Pro.local>
2020-06-17 19:49:59 -0700

    two more left

--- moving_zeroes/moving_zeroes.py
+++ moving_zeroes/moving_zeroes.py
@@ -2,10 +2,30 @@
 Input: a List of integers
 Returns: a List of integers
 '''
+
+
 def moving_zeroes(arr):
     # Your code here
+   #  create a list to store postitive nums and one for negative numbers
+    low = []
+    high = []
 
-    pass
+   # iterate through the list and append numbers to created list
+   # depending if there negative or positive
+    for i in arr:
+        if i <= 0:
+            low.append(i)
+            low.sort()
+        elif i > 0:
+            high.append(i)
+         # check if lists are empty, if they are empty
+         # it means no negative number were found in the list
+        elif len(low) and len(high) == 0:
+         #   return the original list
+            return arr
+   #  otherwise return high and low list
+
+    return high + low
 
 
 if __name__ == '__main__':
